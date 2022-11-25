31481445c69fd6fc2547bc50260314ffa3a86871
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-11-25 10:56:21 -0300

    added js working version

---
+++ index.js
@@ -0,0 +1,18 @@
+async function fetchPKMN(number) {
+  pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/` + number)
+  pokemonJSON = await pokemon.json()
+  return pokemonJSON
+}
+async function main() {
+  let elements = 15
+  let urls = [...Array(elements)].map((item, index) => index + 1)
+  let promises = []
+  // let allPKMN = await Promise.all(urls.map((number) => fetchPKMN(number)))
+  for (let index = 0; index < urls.length; index++) {
+    promises.push(fetchPKMN(index + 1))
+  }
+  let allPKMN = await Promise.all(promises)
+  console.log(allPKMN)
+}
+
+main()
